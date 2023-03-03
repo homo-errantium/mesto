7f0625b3ee0e6d3251febf686ad6bc8cf48985b8
homo-errantium <homo.errantium@gmail.com>
2023-03-04 01:09:19 +0300

    added template cards in js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 const popup = document.querySelector(".popup");
+const places = document.querySelector(".places");
 const profile = document.querySelector(".profile");
 const redactButton = profile.querySelector(".profile__redact-button");
 const closeButton = popup.querySelector(".popup__close");
@@ -9,6 +10,44 @@ const jobInput = popup.querySelector(".popup__input_type_userinfo");
 const usernameProfile = profile.querySelector(".profile__title");
 const userninfoProfile = profile.querySelector(".profile__subtitle");
 const likeLogo = document.querySelector(".places__like-logo");
+
+const initialCards = [
+    {
+        name: "Архыз",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    },
+    {
+        name: "Челябинская область",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+    },
+    {
+        name: "Иваново",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+    },
+    {
+        name: "Камчатка",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    },
+    {
+        name: "Холмогорский район",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    },
+    {
+        name: "Байкал",
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    },
+];
+
+initialCards.forEach(function (card) {
+    const newCard = document
+        .querySelector("#placesCardTemplate")
+        .content.cloneNode(true);
+    const newCardImage = newCard.querySelector(".places__image");
+    newCardImage.setAttribute("src", card.link);
+    const newCardHeading = newCard.querySelector(".places__subtitle");
+    newCardHeading.textContent = card.name;
+    places.append(newCard);
+});
 
 function closePopup() {
     popup.classList.remove("popup_opened");
