e14ba0b8550a7164db5d930846a1d20216cc17e6
homo-errantium <homo.errantium@gmail.com>
2023-03-05 01:02:02 +0300

    added popup add-button function in js

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,21 @@
-const popup = document.querySelector(".popup");
+const popupEdit = document.querySelector(".popup_type-edit");
+const popupAdd = document.querySelector(".popup_type-add");
 const places = document.querySelector(".places");
 const profile = document.querySelector(".profile");
 const redactButton = profile.querySelector(".profile__redact-button");
-const closeButton = popup.querySelector(".popup__close");
-const saveButton = popup.querySelector(".popup__save-button");
-const formPopup = popup.querySelector(".popup__form");
-const nameInput = popup.querySelector(".popup__input_type_username");
-const jobInput = popup.querySelector(".popup__input_type_userinfo");
-const usernameProfile = profile.querySelector(".profile__title");
-const userninfoProfile = profile.querySelector(".profile__subtitle");
+const addButton = profile.querySelector(".profile__add-button");
+const closeButtonEdit = popupEdit.querySelector(".popup__close");
+const closeButtonAdd = popupAdd.querySelector(".popup__close");
+const saveButtonEdit = popupEdit.querySelector(".popup__save-button");
+const creatButtonAdd = popupAdd.querySelector(".popup__save-button");
+const formPopupEdit = popupEdit.querySelector(".popup__form");
+const formPopupAdd = popupAdd.querySelector(".popup__form");
+const nameInputEdit = popupEdit.querySelector(".popup__input_type_username");
+const nameInputAdd = popupAdd.querySelector(".popup__input_type_username");
+const jobInputEdit = popupEdit.querySelector(".popup__input_type_userinfo");
+const linkInputAdd = popupAdd.querySelector(".popup__input_type_userinfo");
+const usernameProfileEdit = profile.querySelector(".profile__title");
+const userninfoProfileEdit = profile.querySelector(".profile__subtitle");
 const likeLogo = document.querySelector(".places__like-logo");
 
 const initialCards = [
@@ -38,7 +45,8 @@ const initialCards = [
     },
 ];
 
-initialCards.forEach(function (card) {
+/*---------------создание карточки--------------*/
+const creatCard = (card) => {
     const newCard = document
         .querySelector("#placesCardTemplate")
         .content.cloneNode(true);
@@ -47,39 +55,56 @@ initialCards.forEach(function (card) {
     const newCardHeading = newCard.querySelector(".places__subtitle");
     newCardHeading.textContent = card.name;
     places.append(newCard);
-});
+};
 
-function closePopup() {
-    popup.classList.remove("popup_opened");
+initialCards.forEach(creatCard);
+
+/*-------------открытие/закрытие попап-а------------------*/
+function closeAddPopup() {
+    popupAdd.classList.remove("popup_opened");
 }
 
-function openPopup() {
-    popup.classList.add("popup_opened");
-    nameInput.value = usernameProfile.textContent;
-    jobInput.value = userninfoProfile.textContent;
+function openAddPopup() {
+    popupAdd.classList.add("popup_opened");
+    // nameInput.value = usernameProfile.textContent;
+    // jobInput.value = userninfoProfile.textContent;
 }
 
-redactButton.addEventListener("click", openPopup);
-closeButton.addEventListener("click", closePopup);
+function closeEditPopup() {
+    popupEdit.classList.remove("popup_opened");
+}
 
+function openEditPopup() {
+    popupEdit.classList.add("popup_opened");
+    nameInputEdit.value = usernameProfileEdit.textContent;
+    jobInputEdit.value = userninfoProfileEdit.textContent;
+}
+
+addButton.addEventListener("click", openAddPopup);
+closeButtonAdd.addEventListener("click", closeAddPopup);
+redactButton.addEventListener("click", openEditPopup);
+closeButtonEdit.addEventListener("click", closeEditPopup);
+
+/*------------------------------добавление данных-------------------*/
 function handleFormSubmit(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    usernameProfile.textContent = nameInput.value;
-    userninfoProfile.textContent = jobInput.value;
-    closePopup();
+    usernameProfileEdit.textContent = nameInputEdit.value;
+    userninfoProfileEdit.textContent = jobInputEdit.value;
+    closeEditPopup();
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formPopup.addEventListener("submit", handleFormSubmit);
+formPopupEdit.addEventListener("submit", handleFormSubmit);
 
-// function activeLikeButton() {
-//     likeLogo.classList.toggle("places__like-logo_active");
-//     if (likeLogo.classList.contains("places__like-logo_active") === true) {
-//         likeLogo.setAttribute("src", "./images/logo-like-active.svg");
-//     } else {
-//         likeLogo.setAttribute("src", "./images/logo-like.svg");
-//     }
-// }
+/*--------------------------лайки-------------------------------*/
+function activeLikeButton() {
+    likeLogo.classList.toggle("places__like-logo_active");
+    if (likeLogo.classList.contains("places__like-logo_active") === true) {
+        likeLogo.setAttribute("src", "./images/logo-like-active.svg");
+    } else {
+        likeLogo.setAttribute("src", "./images/logo-like.svg");
+    }
+}
 
-// likeLogo.addEventListener("click", activeLikeButton);
+likeLogo.addEventListener("click", activeLikeButton);
