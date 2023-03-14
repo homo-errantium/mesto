3f30fea79480008e6f89f6cd156d50bcc0ebd336
homo-errantium <homo.errantium@gmail.com>
2023-03-14 16:32:20 +0300

    add validation on form

--- scripts/index.js
+++ scripts/index.js
@@ -126,3 +126,28 @@ function handleFormSubmitAdd(evt) {
 /*-----обр-к создания/редактирования карточки----*/
 formPopupEdit.addEventListener("submit", handleFormSubmitEdit);
 formPopupAdd.addEventListener("submit", handleFormSubmitAdd);
+
+/*---------------------------*/
+// Функция, которая добавляет класс с ошибкой
+const showInputError = (element) => {
+    element.classList.add("form__input_type_error");
+};
+
+// Функция, которая удаляет класс с ошибкой
+const hideInputError = (element) => {
+    element.classList.remove("form__input_type_error");
+};
+
+// Функция, которая проверяет валидность поля
+const isValid = () => {
+    if (!formInput.validity.valid) {
+        // Если поле не проходит валидацию, покажем ошибку
+        showInputError(formInput);
+    } else {
+        // Если проходит, скроем
+        hideInputError(formInput);
+    }
+};
+
+// Вызовем функцию isValid на каждый ввод символа
+formInput.addEventListener("input", isValid);
